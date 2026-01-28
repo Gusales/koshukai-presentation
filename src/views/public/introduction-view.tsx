bc6f80af33e08a906b40a1c5a36de7f3f2d472a3
devgusales <dev.gussales@gmail.com>
2026-01-28 20:00:57 -0300

    feat: enhance IntroductionView with themes for education, science, and technology; update localization and improve styling

--- src/views/public/introduction-view.tsx
+++ src/views/public/introduction-view.tsx
@@ -9,7 +9,7 @@ export const IntroductionView = () => {
         <PresentationSection key={Steps[0]}>
             <div className="max-w-6xl w-full space-y-12 text-center">
                 <div className="space-y-4">
-                    <h1 className="text-5xl md:text-7xl tracking-tighter text-gray-700">
+                    <h1 className="text-5xl md:text-7xl tracking-tighter text-gray-900">
                     { t('introduction.title') }
                     </h1>
                     <p className="text-xl md:text-2xl text-gray-500">
@@ -17,19 +17,67 @@ export const IntroductionView = () => {
                     </p>
                 </div>
 
-                <div>
-                    <div className="relative w-full h-64 md:h-96 rounded-md overflow-hidden">
-                        <img 
-                            src="https://images.unsplash.com/photo-1637071690952-039888a82b0f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxrb2klMjBmaXNoJTIwamFwYW5lc2UlMjBhcnR8ZW58MXx8fHwxNzY5MDM5NjM3fDA&ixlib=rb-4.1.0&q=80&w=1080"
-                            alt="Koi Fish Art"
-                            className="w-full h-full object-cover opacity-60"
-                        />
+                <div className="grid md:grid-cols-3 gap-4">
+                    <article className="relative w-full h-48 md:h-64 rounded-md overflow-hidden group">
+                        <figure className="absolute inset-0">
+                            <img 
+                                src="https://www.europeanbusinessreview.com/wp-content/uploads/2024/05/iStock-1131423516-1.jpg"
+                                alt="Koi Fish Art"
+                                className="w-full h-full object-cover group-hover:scale-110 transition-all duration-300"
+                                loading="lazy"
+                            />
 
-                        <div className="absolute inset-0 bg-linear-to-t from-[#f8f7f4] via-transparent to-transparent" />
-                    </div>
+                            <div className="absolute inset-0 bg-linear-to-t from-black via-transparent to-transparent" />
+                        </figure>
+
+                        <div className="absolute bottom-4 left-4 right-4 text-white">
+                            <p className="text-lg font-medium">
+                                { t('introduction.themes.education') }
+                            </p>
+                        </div>
+                    </article>
+                    
+                    <article className="relative w-full h-48 md:h-64 rounded-md overflow-hidden group">
+                        <figure className="absolute inset-0">
+                            <img 
+                                src="https://s3.amazonaws.com/assets.dotlib.com/public/images/ranking-de-melhores-cientistas-do-mundo-tem-10-pesquisadores-do-brasil.png"
+                                alt="Koi Fish Art"
+                                className="w-full h-full object-cover group-hover:scale-110 transition-all duration-300"
+                                loading="lazy"
+                            />
+
+                            <div className="absolute inset-0 bg-linear-to-t from-black via-transparent to-transparent" />
+                        </figure>
+
+                        <div className="absolute bottom-4 left-4 right-4 text-white">
+                            <p className="text-lg font-medium">
+                                { t('introduction.themes.science') }
+                            </p>
+                        </div>
+                    </article>
+
+                    <article className="relative w-full h-48 md:h-64 rounded-md overflow-hidden group">
+                        <figure className="absolute inset-0">
+                            <img 
+                                src="https://thumbs.dreamstime.com/b/digital-map-south-america-brazil-highlighted-glowing-neon-blue-lines-dots-global-network-connectivity-concept-abstract-405871789.jpg"
+                                alt="Koi Fish Art"
+                                className="w-full h-full object-cover group-hover:scale-110 transition-all duration-300"
+                                loading="lazy"
+                            />
+
+                            <div className="absolute inset-0 bg-linear-to-t from-black via-transparent to-transparent" />
+                        </figure>
+
+
+                        <div className="absolute bottom-4 left-4 right-4 text-white">
+                            <p className="text-lg font-medium">
+                                { t('introduction.themes.technology') }
+                            </p>
+                        </div>
+                    </article>
                 </div>
 
-                <p className="text-3xl text-gray-500">
+                <p className="text-3xl text-gray-800">
                     {t('introduction.action')}
                 </p>
             </div>
