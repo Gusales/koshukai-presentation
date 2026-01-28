081cf669164eb4aa75536b5b0203481ee2531989
devgusales <dev.gussales@gmail.com>
2026-01-28 07:31:38 -0300

    feat: add WebSocket connection to StepContextProvider; manage connection state and handle incoming messages

--- src/contexts/step-context.tsx
+++ src/contexts/step-context.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import { createContext, useContext, type Dispatch, type ReactNode, type SetStateAction } from "react";
+import { createContext, useContext, useEffect, useRef, type Dispatch, type ReactNode, type SetStateAction } from "react";
 
 import { useState } from "react";
 import { Steps } from "../contants";
@@ -7,6 +7,7 @@ import type { StepType } from "../types/step.type";
 
 export interface StepContextInterface {
     step: StepType,
+    connected: boolean
     stepPosition: number
     nextStep: () => void
     previousStep: () => void
@@ -17,18 +18,57 @@ export const StepContext = createContext<StepContextInterface>({} as StepContext
 
 export const StepContextProvider = ({ children }: { children: ReactNode }) => {
     const [step, setStep] = useState<number>(0);
+    const [isReady, setIsReady] = useState(false)
+    const webSocket = useRef<WebSocket | null>(null)
+
+    useEffect(() => {
+        function connect() {
+            const webSocketUrl = "ws://127.0.1.1:8756"
+            try {
+                const ws = new WebSocket(webSocketUrl)
+
+                ws.onopen = () => setIsReady(true)
+
+                ws.onclose = () => {
+                    setIsReady(false)
+                    console.warn('Conexão com servidor WebSocket encerrada.')
+                }
+
+                ws.onmessage = ({ data }) => {
+                    console.log("Mensagem recebida")
+                    console.log(data)
+
+                    setStep(Number(data))
+                }
+
+                webSocket.current = ws
+
+            } catch (error) {
+                console.log('Não foi possível se conectar com o servidor WebSocket. A apresentação não será assistida.')
+                console.log(JSON.stringify(error, null, 2))
+
+                setIsReady(false)
+            }
+        }
+
+        connect()
+    }, [])
 
     function nextStep() {
         setStep((current) => {
             if (current === 6) return current;
-            return current += 1;
+            const newStep = current += 1;
+            webSocket.current?.send(JSON.stringify(newStep))
+            return newStep;
         })
     }
 
     function previousStep() {
         setStep((current) => {
             if (current === 1) return current;
-            return current -= 1;
+            const newStep = current -= 1;
+            webSocket.current?.send(JSON.stringify(newStep))
+            return newStep;
         })
     }
 
@@ -36,6 +76,7 @@ export const StepContextProvider = ({ children }: { children: ReactNode }) => {
         step: Steps[step],
         stepPosition: step,
         setStep,
+        connected: isReady,
         nextStep,
         previousStep
     }
